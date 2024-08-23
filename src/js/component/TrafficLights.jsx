37677b150d8684776dd3b9ddb04e2b9a40c2d19a
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2024-08-23 18:23:06 +0000

    add: useState color implements

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TrafficLight = () => {
+    const [color, setColor] = useState("red");
 
     return (
         <div className="container my-5">
             <div className="container bg-dark p-3 rounded">
-                <div className={ "light" }> </div>
-                <div className={ "light" }> </div>
-                <div className={ "light" }> </div>
+                <div className={ "light red " + (color == "red" ? "ligthOn" : "")}
+                        onClick={() => setColor("red")}>
+                </div>
+
+                <div
+                    className={ "light yellow " + (color == "yellow" ? "ligthOn" : "")}
+                    onClick={() => setColor("yellow")}>
+                </div>
+
+                <div
+                    className={ "light green " + (color == "green" ? "ligthOn" : "")}
+                    onClick={() => setColor("green")}>
+                </div>
             </div>
         </div>
     );
