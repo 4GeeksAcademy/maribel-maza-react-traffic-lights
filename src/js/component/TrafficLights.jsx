8a439999cbe8674888c5f94f0256b5b13e336c76
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2024-08-24 01:45:36 +0000

    subiendo proyecto

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -5,18 +5,19 @@ export const TrafficLight = () => {
 
     return (
         <div className="container my-5">
+            <div className="containerTop" ></div>
             <div className="container bg-dark p-3 rounded">
-                <div className={ "light red " + (color == "red" ? "ligthOn" : "")}
-                        onClick={() => setColor("red")}>
+                <div className={"light red " + (color == "red" ? "lightOn" : "")}
+                    onClick={() => setColor("red")}>
                 </div>
 
                 <div
-                    className={ "light yellow " + (color == "yellow" ? "ligthOn" : "")}
+                    className={"light yellow " + (color == "yellow" ? "lightOn" : "")}
                     onClick={() => setColor("yellow")}>
                 </div>
 
                 <div
-                    className={ "light green " + (color == "green" ? "ligthOn" : "")}
+                    className={"light green " + (color == "green" ? "lightOn" : "")}
                     onClick={() => setColor("green")}>
                 </div>
             </div>
